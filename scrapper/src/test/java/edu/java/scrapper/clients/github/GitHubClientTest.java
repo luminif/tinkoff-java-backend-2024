aba7ba421fbeeedda74f8d3bc18990d1204da40d
luminif <arcocs72717@mail.ru>
2024-02-25 18:26:20 +0300

    Added tests

---
+++ scrapper/src/test/java/edu/java/scrapper/clients/github/GitHubClientTest.java
@@ -0,0 +1,56 @@
+package edu.java.scrapper.clients.github;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import edu.java.clients.github.*;
+import java.time.OffsetDateTime;
+
+public class GitHubClientTest {
+    private WireMockServer wireMockServer;
+    private GitHubClientInterface gitHubClient;
+
+    @BeforeEach
+    void setUp() {
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+        WireMock.configureFor("localhost", wireMockServer.port());
+        String baseUrl = "http://localhost:" + wireMockServer.port();
+        gitHubClient = new GitHubClient(baseUrl);
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void equalBodyTest() {
+        String username = "luminif";
+        String repository = "tinkoff-2024";
+        String body = "{\"id\":1337,\"name\":\"luminif\",\"updated_at\":\"2024-02-24T18:00:00Z\",\"pushed_at\":\"2024-02-24T18:02:05Z\"}";
+
+        stubFor(get(urlEqualTo("/repos/%s/%s".formatted(username, repository)))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .withBody(body))
+            );
+
+        GitHubResponse gitHubResponse = gitHubClient.fetchRepository(username, repository);
+
+        assertEquals(1337, gitHubResponse.id());
+        assertEquals("luminif", gitHubResponse.name());
+        assertEquals(OffsetDateTime.parse("2024-02-24T18:00:00Z"), gitHubResponse.updatedAt());
+        assertEquals(OffsetDateTime.parse("2024-02-24T18:02:05Z"), gitHubResponse.pushedAt());
+    }
+}
---
+++ scrapper/src/test/java/edu/java/scrapper/clients/stackoverflow/StackOverflowClientTest.java
@@ -0,0 +1,60 @@
+package edu.java.scrapper.clients.stackoverflow;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import edu.java.clients.github.GitHubClient;
+import edu.java.clients.github.GitHubClientInterface;
+import edu.java.clients.github.GitHubResponse;
+import edu.java.clients.stackoverflow.StackOverflowClient;
+import edu.java.clients.stackoverflow.StackOverflowClientInterface;
+import edu.java.clients.stackoverflow.StackOverflowResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import java.time.OffsetDateTime;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StackOverflowClientTest {
+    private WireMockServer wireMockServer;
+    private StackOverflowClientInterface stackOverflowClient;
+
+    @BeforeEach
+    void setUp() {
+        wireMockServer = new WireMockServer();
+        wireMockServer.start();
+        WireMock.configureFor("localhost", wireMockServer.port());
+        String baseUrl = "http://localhost:" + wireMockServer.port();
+        stackOverflowClient = new StackOverflowClient(baseUrl);
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void equalBodyTest() {
+        String username = "luminif";
+        long postId = 1337;
+        String body = "{\"name\":\"luminif\",\"last_activity_date\":\"2024-02-24T18:20:00Z\",\"post_id\":1337}";
+
+        stubFor(get(urlEqualTo("/questions/%d&site=stackoverflow".formatted(postId)))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .withBody(body))
+        );
+
+        StackOverflowResponse stackOverflowResponse = stackOverflowClient.fetchQuestion(postId);
+
+        assertEquals("luminif", stackOverflowResponse.name());
+        assertEquals(OffsetDateTime.parse("2024-02-24T18:20:00Z"), stackOverflowResponse.lastActivityDate());
+        assertEquals(1337, stackOverflowResponse.postId());
+    }
+}
