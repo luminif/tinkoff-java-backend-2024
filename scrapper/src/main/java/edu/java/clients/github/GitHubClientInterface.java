a198b2a99ee183f89723fe57d479eb06f5f6a90e
luminif <arcocs72717@mail.ru>
2024-03-24 00:01:23 +0300

    Processing new github commits

--- scrapper/src/main/java/edu/java/clients/github/GitHubClientInterface.java
+++ scrapper/src/main/java/edu/java/clients/github/GitHubClientInterface.java
@@ -1,5 +1,10 @@
 package edu.java.clients.github;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 public interface GitHubClientInterface {
     GitHubResponse fetchRepository(String username, String repositoryName);
+
+    List<GitHubCommitResponse> fetchCommit(String username, String repositoryName, OffsetDateTime date);
 }
--- scrapper/src/main/java/edu/java/clients/handlers/GitHubClientHandler.java
+++ scrapper/src/main/java/edu/java/clients/handlers/GitHubClientHandler.java
@@ -1,8 +1,11 @@
 package edu.java.clients.handlers;
 
 import edu.java.clients.github.GitHubClient;
+import edu.java.clients.github.GitHubCommitResponse;
 import edu.java.clients.github.GitHubResponse;
 import edu.java.dao.JdbcLinkDao;
+import java.time.OffsetDateTime;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -24,13 +27,25 @@ public class GitHubClientHandler implements ClientHandler {
         String[] segments = link.split("/+");
         String name = segments[NAME_INDEX];
         String repository = segments[REP_INDEX];
+        OffsetDateTime lastUpdate = jdbcLinkDao.getLastUpdate(link);
+        var text = new StringBuilder();
 
         GitHubResponse response = gitHubClient.fetchRepository(name, repository);
 
-        if (response.pushedAt().isAfter(jdbcLinkDao.getLastUpdate(link))) {
-            return "обновление в репозитории %s".formatted(link);
+        if (response.pushedAt().isAfter(lastUpdate)) {
+            text.append("обновление в репозитории %s".formatted(link)).append("\n");
         }
 
-        return EMPTY;
+        List<GitHubCommitResponse> commitResponses = gitHubClient.fetchCommit(name, repository, lastUpdate);
+        int commitsCount = 0;
+
+        for (var commitResponse : commitResponses) {
+            if (commitResponse.commit().author().date().isAfter(lastUpdate)) {
+                commitsCount++;
+            }
+        }
+
+        text.append("в репозитории %s новый коммит".formatted(link)).append(commitsCount == 1 ? "" : "ы").append("\n");
+        return text.toString();
     }
 }
