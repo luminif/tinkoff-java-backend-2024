9fba0486683d92d0fdbf7828c5b0aa65e61d9a91
luminif <arcocs72717@mail.ru>
2024-04-14 00:49:19 +0300

    Checkstyle fix

--- bot/src/main/java/edu/java/bot/services/ScrapperQueueListener.java
+++ bot/src/main/java/edu/java/bot/services/ScrapperQueueListener.java
@@ -3,11 +3,11 @@ package edu.java.bot.services;
 import edu.java.bot.api.components.LinkUpdateRequest;
 import edu.java.bot.configuration.ApplicationConfig;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Service
 @RequiredArgsConstructor
